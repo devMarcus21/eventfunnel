125817ad92d7676f65ba859bbb55cce74ff3f1f3
devMarcus21 <marcusaleclee2000@gmail.com>
2023-05-04 10:43:04 -0700

    Changing schema property of event to model

--- pkg/utils/event/event.go
+++ pkg/utils/event/event.go
@@ -2,7 +2,7 @@ package event
 
 type Event struct {
 	PartitionKey string         `json:"partitionKey"`
-	Schema       string         `json:"schema"`
+	Model       string         `json:"model"`
 	EventId      string         `json:"eventId"`
 	Timestamp    int64          `json:"timestamp"`
 	Stage        string         `json:"stage"`

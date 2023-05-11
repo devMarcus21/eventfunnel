884b7b30d380dbd05ffcbbb3ea03adba1de6e3b8
devMarcus21 <marcusaleclee2000@gmail.com>
2023-05-11 11:37:00 -0700

    Converting type of scheme from model table to a map type

--- pkg/utils/dbutils/model/model.go
+++ pkg/utils/dbutils/model/model.go
@@ -3,5 +3,5 @@ package model
 type Model struct {
 	Name   string         `json:"name"`
 	Stage  string         `json:"stage"`
-	Scheme map[string]any `json:"scheme"`
+	Scheme map[string]any `json:"scheme"` // saved in cassandra models table as a string
 }
--- pkg/utils/dbutils/model/modeltablefactory.go
+++ pkg/utils/dbutils/model/modeltablefactory.go
@@ -5,13 +5,17 @@ import (
 )
 
 func deserializeMapToModel(model map[string]any) Model {
-	var serializedModel Model
+	// saved in cassandra models table as a string so need to convert it to proper type
+	var scheme map[string]any
+	// TODO handle errors
+	json.Unmarshal([]byte(model["scheme"].(string)), &scheme)
 
-	// TODO handle possible errors
-	jsonData, _ := json.Marshal(model)
-	json.Unmarshal(jsonData, &serializedModel)
-
-	return serializedModel
+	// TODO handle type checking here
+	return Model{
+		Name:   model["name"].(string),
+		Stage:  model["stage"].(string),
+		Scheme: scheme,
+	}
 }
 
 func CreateModelTable(cassandra func(params ...interface{}) []map[string]any) func(string, string) Model {

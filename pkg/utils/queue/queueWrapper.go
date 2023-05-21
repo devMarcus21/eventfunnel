4efcea0e536a375a48146493e1d12d9ecd000687
devMarcus21 <marcusaleclee2000@gmail.com>
2023-05-20 22:47:40 -0700

    Added comment for batching

--- pkg/utils/queue/queueWrapper.go
+++ pkg/utils/queue/queueWrapper.go
@@ -5,6 +5,7 @@ import (
 	rabbitmqwrapper "github.com/devMarcus21/eventfunnel/pkg/utils/queue/rabbitmqwrapper"
 )
 
+// TODO add function batching support for this client. Rabbitmq itself may not support batching
 func GetQueuePublisherWrapper(queueServiceConnection string, queueName string) func(event.Event) bool {
 	return rabbitmqwrapper.GetRabbitmqPublisherWrapper(queueServiceConnection, queueName)
 }
